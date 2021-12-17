5bc9c1dc8f1f7037ad783c00d462b949f0b268a3
Lannadelrey150 <lanna.j.gale@gmail.com>
2021-12-16 17:49:21 -0800

    Uploaded the Story Time JS file or basic data type

---
+++ Basic Data Types Activity/LannaGale_story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Hello I'm ";
+let name = "Steven Universe";
+let title = " Crystal Gem";
+let cityName = " Beach City";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 16;
+let age2 = 12;
+let year = 2018;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let FriendsAndWeapons = ['Garnet','Amythest','Pearl','Lion','Rose Shield','Rose Sword'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let favAnimal = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log(greetings + name + ".");
+console.log("I was part of an alien rebellion called the" + title + "s." + " And my mother was their leader.");
+console.log("All of my life, I lived in " + cityName + " in a van with my dad, until I was " + age2 + " years old and moved into the temple with the gems. ");
+console.log("Currently there are four" + title + "s ," + FriendsAndWeapons[0] +", " + FriendsAndWeapons[1] +", "+ FriendsAndWeapons[2] +", and me!" );
+console.log("When I was younger, I struggled with creating my weapons with the use of my powers.");
+console.log("It took me a year to learn how to use my gem. I was able to fortify a" + FriendsAndWeapons[4] + " and " + FriendsAndWeapons[5] + ".");
+console.log("They became very useful in the year of " + year + " when mutant gems became a major threat to our planet.");
+console.log("But don't get me wrong, it is " +favAnimal+ " these times were hard, but luckily I had my pink pet " + FriendsAndWeapons[3] + " get us out of tight situations.");
